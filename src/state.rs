3d8e287a12318c08bb68d4296c16fe2eaf08ca5b
Nicholas Kariniemi <nicholas.kariniemi@gmail.com>
2016-01-17 01:36:23 +0200

    Add player shares

--- src/state.rs
+++ src/state.rs
@@ -10,7 +10,19 @@ pub struct Game {
 
 #[derive(RustcDecodable, RustcEncodable)]
 struct Player {
-  money: i32
+  money: i32,
+  shares: PlayerShares
+}
+
+#[derive(RustcDecodable, RustcEncodable)]
+struct PlayerShares {
+  luxor: i8,
+  tower: i8,
+  american: i8,
+  festival: i8,
+  worldwide: i8,
+  continental: i8,
+  imperial: i8
 }
 
 #[derive(RustcDecodable, RustcEncodable)]
@@ -26,10 +38,18 @@ struct Slot {
 #[derive(RustcDecodable, RustcEncodable)]
 enum Hotel { Luxor, Tower, American, Festival, Worldwide, Continental, Imperial }
 
+fn emptyShares() -> PlayerShares {
+  PlayerShares { luxor: 0, tower: 0, american: 0, festival: 0, worldwide: 0, continental: 0, imperial: 0 }
+}
+
+fn newPlayer() -> Player {
+  Player { money: 0, shares: emptyShares() }
+}
+
 pub fn initialState() -> Game {
   let slots = (0..COLS)
     .map(|x| (0..ROWS).map(|x| Slot { hotel: None } ).collect() )
     .collect();
-  let players = (0..PLAYERS).map(|x| Player { money: 0 } ).collect();
+  let players = (0..PLAYERS).map(|x| newPlayer() ).collect();
   Game { board: Board { slots: slots }, players: players }
 }
