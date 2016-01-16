a21d1d8c07792791979539fba4a66a7ad2a7cb20
d3k4r <d3k4r@users.noreply.github.com>
2016-01-16 23:53:19 +0200

    Get hyper http server up and responding.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
+extern crate hyper;
+
+use hyper::server::{Server, Request, Response};
+
+fn hello(req: Request, res: Response) {
     println!("Hey, what up.");
+}
+
+fn main() {
+	Server::http("localhost:3001").unwrap().handle(hello).unwrap();
 }
