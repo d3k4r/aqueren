1930e0208ee29f044b8339515f43b3893ef17563
d3k4r <d3k4r@users.noreply.github.com>
2016-01-17 13:32:03 +0200

    Format spacing based on rust-lang/rust.vim

--- src/main.rs
+++ src/main.rs
@@ -12,33 +12,33 @@ macro_rules! try_return(
     ($e:expr) => {{
         match $e {
             Ok(v) => v,
-	    Err(e) => { println!("Error: {}", e); return; }
-	}
+            Err(e) => { println!("Error: {}", e); return; }
+        }
     }}
-);
+    );
 
 fn handle_request(req: Request, mut res: Response) {
-	match req.uri {
-		AbsolutePath(ref path) => match (&req.method, &path[..]) {
-			(&Get, "/") => {
-				try_return!(res.send(b"Hey, what up."));
-				return;
-			},
-			(&Get, "/state") => {
-				let game = state::initialState();
-				let encoded = json::encode(&game).unwrap();
-				try_return!(res.send(encoded.as_bytes()));
-				return;
-			}
-			_ => {
-				*res.status_mut() = hyper::NotFound;
-				return;
-			}
-		},
-		_ => {
-			return;
-		}
-	};
+    match req.uri {
+        AbsolutePath(ref path) => match (&req.method, &path[..]) {
+            (&Get, "/") => {
+                try_return!(res.send(b"Hey, what up."));
+                return;
+            },
+            (&Get, "/state") => {
+                let game = state::initialState();
+                let encoded = json::encode(&game).unwrap();
+                try_return!(res.send(encoded.as_bytes()));
+                return;
+            }
+            _ => {
+                *res.status_mut() = hyper::NotFound;
+                return;
+            }
+        },
+        _ => {
+            return;
+        }
+    };
 }
 
 fn main() {
--- src/state.rs
+++ src/state.rs
@@ -4,52 +4,52 @@ const PLAYERS: i8 = 4;
 
 #[derive(RustcDecodable, RustcEncodable)]
 pub struct Game {
-  players: Vec<Player>,
-  board: Board
+    players: Vec<Player>,
+    board: Board
 }
 
 #[derive(RustcDecodable, RustcEncodable)]
 struct Player {
-  money: i32,
-  shares: PlayerShares
+    money: i32,
+    shares: PlayerShares
 }
 
 #[derive(RustcDecodable, RustcEncodable)]
 struct PlayerShares {
-  luxor: i8,
-  tower: i8,
-  american: i8,
-  festival: i8,
-  worldwide: i8,
-  continental: i8,
-  imperial: i8
+    luxor: i8,
+    tower: i8,
+    american: i8,
+    festival: i8,
+    worldwide: i8,
+    continental: i8,
+    imperial: i8
 }
 
 #[derive(RustcDecodable, RustcEncodable)]
 struct Board {
-  slots: Vec<Vec<Slot>>
+    slots: Vec<Vec<Slot>>
 }
 
 #[derive(RustcDecodable, RustcEncodable)]
 struct Slot {
-  hotel: Option<Hotel>
+    hotel: Option<Hotel>
 }
 
 #[derive(RustcDecodable, RustcEncodable)]
 enum Hotel { Luxor, Tower, American, Festival, Worldwide, Continental, Imperial }
 
 fn emptyShares() -> PlayerShares {
-  PlayerShares { luxor: 0, tower: 0, american: 0, festival: 0, worldwide: 0, continental: 0, imperial: 0 }
+    PlayerShares { luxor: 0, tower: 0, american: 0, festival: 0, worldwide: 0, continental: 0, imperial: 0 }
 }
 
 fn newPlayer() -> Player {
-  Player { money: 0, shares: emptyShares() }
+    Player { money: 0, shares: emptyShares() }
 }
 
 pub fn initialState() -> Game {
-  let slots = (0..COLS)
-    .map(|x| (0..ROWS).map(|x| Slot { hotel: None } ).collect() )
-    .collect();
-  let players = (0..PLAYERS).map(|x| newPlayer() ).collect();
-  Game { board: Board { slots: slots }, players: players }
+    let slots = (0..COLS)
+        .map(|x| (0..ROWS).map(|x| Slot { hotel: None } ).collect() )
+        .collect();
+    let players = (0..PLAYERS).map(|x| newPlayer() ).collect();
+    Game { board: Board { slots: slots }, players: players }
 }
