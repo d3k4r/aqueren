f282b5feec92958ebc29e38d57c748376a348a33
d3k4r <d3k4r@users.noreply.github.com>
2016-01-23 17:35:06 +0200

    Add /turn POST API with stubbed in response of the req.body

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,10 @@ extern crate rustc_serialize;
 
 mod state;
 
+use std::io::Read;
 use rustc_serialize::json;
-use hyper::Get;
+use hyper::{Get, Post};
+use hyper::header::ContentLength;
 use hyper::server::{Server, Request, Response};
 use hyper::uri::RequestUri::AbsolutePath;
 
@@ -18,28 +20,58 @@ macro_rules! try_return(
     }}
     );
 
-fn handle_request(req: Request, mut res: Response) {
-    match req.uri {
-        AbsolutePath(ref path) => match (&req.method, &path[..]) {
-            (&Get, "/") => {
-                try_return!(res.send(b"Hey, what up."));
-                return;
-            },
-            (&Get, "/state") => {
-                let game = state::initial_state();
-                let encoded = json::encode(&game).unwrap();
-                try_return!(res.send(encoded.as_bytes()));
-                return;
+fn handle_request(mut req: Request, mut res: Response) {
+    let mut body: String  = "".to_string();
+    req.read_to_string(&mut body);
+
+    let path = match req.uri {
+        AbsolutePath(ref path) => {
+            path
+        },
+        _ => panic!("Unsupported URI format."),
+    };
+
+    match path.as_ref() {
+        "/" => {
+            match req.method {
+                Get => {
+                    try_return!(res.send(b"Hey, what up."));
+                },
+                _ => {
+                    *res.status_mut() = hyper::BadRequest;
+                    (*res.headers_mut()).set(ContentLength(0));
+                }
             }
-            _ => {
-                *res.status_mut() = hyper::NotFound;
-                return;
+        },
+        "/state" => {
+            match req.method {
+                Get => {
+                    let game = state::initial_state();
+                    let encoded = json::encode(&game).unwrap();
+                    try_return!(res.send(encoded.as_bytes()));
+                },
+                _ => {
+                    *res.status_mut() = hyper::BadRequest;
+                    (*res.headers_mut()).set(ContentLength(0));
+                }
+            }
+        },
+        "/turn" => {
+            match req.method {
+                Post => {
+                    try_return!(res.send(body.as_bytes()));
+                },
+                _ => {
+                    *res.status_mut() = hyper::BadRequest;
+                    (*res.headers_mut()).set(ContentLength(0));
+                }
             }
         },
         _ => {
-            return;
+            *res.status_mut() = hyper::NotFound;
+            (*res.headers_mut()).set(ContentLength(0));
         }
-    };
+    }
 }
 
 fn main() {
